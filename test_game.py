880415bca369ca3ac0bc284f7724895b7b4928a0
jihye <jhlov.jin@samsung.com>
2024-06-13 13:58:09 +0900

    [refactoring] 27

--- test_game.py
+++ test_game.py
@@ -2,7 +2,10 @@ from unittest import TestCase
 from game import Game
 
 class TestGame(TestCase):
-    def test_exception_when_input_is_none(self):
+    def setUp(self):
+        super().setUp()
         self.game = Game()
+    def test_exception_when_input_is_none(self):
         with self.assertRaises(TypeError):
             self.game.guess(None)
+
