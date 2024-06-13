317d5d8155a241cfed0cf5660755642859175e05
jihye <jhlov.jin@samsung.com>
2024-06-13 14:08:24 +0900

    [refactoring] 42

--- game.py
+++ game.py
@@ -1,14 +1,17 @@
 class Game:
     def guess(self, guessNumber):
+        self.assert_illegal_value(guessNumber)
+
+    def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
             raise TypeError()
-
         if len(guessNumber) != 3:
             raise TypeError()
-
         for number in guessNumber:
             if not ord('0') <= ord(number) <= ord('9'):
                 raise TypeError()
-
-        if len(set(guessNumber)) != 3:
+        if self.isDuplicaedNumber(guessNumber):
             raise TypeError()
+
+    def isDuplicaedNumber(self, guessNumber):
+        return len(set(guessNumber)) != 3
